6afd5d0063e67dca47646d37db030cbd4b81d630
Ing-CristianLuna <ing.cris.luna@gmail.com>
2026-04-20 13:53:28 -0600

    Actualiza axios.api.jsx 2

--- Frontend/src/api/axios.api.jsx
+++ Frontend/src/api/axios.api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({ import.meta.env.VITE_API_URL });
+export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 
 //para que mande el token en cada petiion
